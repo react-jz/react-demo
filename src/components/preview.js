e0bbbdab8112e0cfad62fcfb6a35a2b0df3a205c
丑晴 <chouqing@reignwood.com>
2017-08-15 15:49:27 +0800

    react组件格式修改

--- src/components/preview.js
+++ src/components/preview.js
@@ -1,20 +1,33 @@
-import react from 'react'
+import react, { Component } from 'react'
 import { PreviewFooter, PreviewItem, PreviewButton } from 'react-weui'
 
-const PreviewDemo = (props) => (
-    <div className="preview" title="Preview" subTitle="表单预览">
-        <Preview>
-            <PreviewHeader>
-                <PreviewItem label="Total" value="$49.99" />
-            </PreviewHeader>
-            <PreviewBody>
-                <PreviewItem label="Product" value="Name" />
-                <PreviewItem label="Description" value="Product Description" />
-                <PreviewItem label="Details" value="Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. " />
-            </PreviewBody>
-            <PreviewFooter>
-                <PreviewButton primary>Action</PreviewButton>
-            </PreviewFooter>
-        </Preview>
-    </div>
-)
+class PreView extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="preview" title="Preview" subTitle="表单预览">
+                    <Preview>
+                        <PreviewHeader>
+                            <PreviewItem label="Total" value="$49.99" />
+                        </PreviewHeader>
+                        <PreviewBody>
+                            <PreviewItem label="Product" value="Name" />
+                            <PreviewItem label="Description" value="Product Description" />
+                            <PreviewItem label="Details" value="Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. " />
+                        </PreviewBody>
+                        <PreviewFooter>
+                            <PreviewButton primary>Action</PreviewButton>
+                        </PreviewFooter>
+                    </Preview>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default PreView
