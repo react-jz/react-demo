36a9ae21405aa3ee24ccfdb482ec2dfad93dcf10
丑晴 <chouqing@reignwood.com>
2017-08-15 17:21:38 +0800

    first

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PeopleContainer from './components/PeopleContainer';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+            <h2>7</h2>
+        </div>
+        <p className="App-intro">
+          hello
+        </p>
+        <PeopleContainer />
+      </div>
+    );
+  }
+}
+
+export default App;
